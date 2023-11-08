a7e249bc2944fd5bb1c7a1a065d1b9f8b488ab1c
Suhas D D <102498520+Suhasdd@users.noreply.github.com>
2023-11-08 12:10:26 +0530

    Update whl2.sh

--- whl2.sh
+++ whl2.sh
@@ -1,5 +1,5 @@
 #!\bin\bash
-echo enter the number
+echo "enter the number"
 read n
 fact=1
 while [ $n -gt 1 ]
@@ -7,4 +7,4 @@ do
         fact=`expr $fact \* $n`
         n=`expr $n - 1`
 done
-echo the factorial of the number is $fact
+echo "the factorial of the number is $fact"
