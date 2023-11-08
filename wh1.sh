0818309ca1b3e19e1d1feb8bb7853f109fe809c9
Suhas D D <102498520+Suhasdd@users.noreply.github.com>
2023-11-08 12:09:51 +0530

    Update wh1.sh

--- wh1.sh
+++ wh1.sh
@@ -1,5 +1,5 @@
 #!\bin\bash
-echo enter the number
+echo "enter the number"
 read n
 sum=0
 while [ $n -gt 0 ]
@@ -7,4 +7,4 @@ do
         sum=`expr $sum + $n`
         n=`expr $n - 1 `
         done
-        echo the sum is $sum
+        echo "the sum is $sum"
