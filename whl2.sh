7ae80548b4faa7e3068e73092666b7c1a4098a0d
Suhas D D <102498520+Suhasdd@users.noreply.github.com>
2023-11-08 12:06:26 +0530

    Create whl2.sh

---
+++ whl2.sh
@@ -0,0 +1,10 @@
+#!\bin\bash
+echo enter the number
+read n
+fact=1
+while [ $n -gt 1 ]
+do
+        fact=`expr $fact \* $n`
+        n=`expr $n - 1`
+done
+echo the factorial of the number is $fact
